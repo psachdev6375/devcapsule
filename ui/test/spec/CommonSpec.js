086aa604652f14cc870304b417209b407530ffc0
vagrant <vagrant@precise64.(none)>
2017-01-17 22:40:24 +0530

    Devcapsule Checkin

---
+++ ui/test/spec/CommonSpec.js
@@ -0,0 +1,36 @@
+'use strict';
+
+describe('Testing Common JS Functions', function () {
+  // var spyEvent;
+  // beforeEach(function(){
+  //    // setFixtures('<form id="testForm" action="">'
+  //    //            +'  <h1>Test Form</h1>'
+  //    //            +'  <input type="text" id="txtMessage">'
+  //    //            +'  <br>'
+  //    //            +'  <button id="btnHideMessage" type="button" onclick="">Hide Message</button>'
+  //    //            +'  <button id="btnShowMessage" type="button" onclick="">Show Message</button>'
+  //    //            +'  <br>'
+  //    //            +'  <p id="pMsg"></p>'
+  //    //            +'</form>');
+  //   loadFixtures('pizzas.html')
+  // });
+  //  it ("should invoke the edit pizza event.", function() {
+  //   spyEvent = spyOnEvent('#edit1', 'click');
+  //   $('#edit1').trigger( "click" );
+
+  //   expect('click').toHaveBeenTriggeredOn('#edit1');
+  //   expect(spyEvent).toHaveBeenTriggered();
+  // });
+
+  // // it ("should invoke the pizzaModal click event.", function() {
+  // //   spyEvent = spyOnEvent('#pizzaModal', 'click');
+  // //   $('#pizzaModal').trigger( "click" );
+
+  // //   expect('click').toHaveBeenTriggeredOn('#pizzaModal');
+  // //   expect(spyEvent).toHaveBeenTriggered();
+  // // });
+
+
+})
+  
+;
